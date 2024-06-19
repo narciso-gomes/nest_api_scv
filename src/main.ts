0af6891cfc9705810140a8a438b03b8dd868f59c
narcisogomes <narcisoalmeida25@gmail.com>
2024-06-18 23:55:49 -0300

    feature/ add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpExceptionFilter } from './error/http-exception.filter';
 import { ErrorsInterceptor } from './error/errors.interceptor';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 require('../patch.js');
 
@@ -9,6 +10,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
   app.useGlobalInterceptors(new ErrorsInterceptor());
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats api description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
